4ea38fb355f0be22f59c247a949f7e5cfce968d7
LAPTOP-JAD1OL75\User <dream7652210@gmail.com>
2018-04-12 14:05:08 +0800

    剩下找星座

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,8 +12,23 @@ namespace ConsoleApp1
         {
 
             string[] Type = { "O", "其他", "O", "O", "A", "O", "A", "A", "O", "O", "A", "O", "A", "B", "O", "O", "A", "其他", "O", "O", "A", "", "B", "O", "", "O", "B", "O", "B", "B", "B", "O", "O", "AB", "O", "B", "A", "O", "O", "", "O", "A", "", "O", "O", "A", "O", "O", "其他", "B", "O", "O", "O", "A", "AB", "A", "O", "B", "AB", "", "O", "O", "O", "", "O", "A", "A", "O" };
+            string[] Gender = { "男", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "男", "女", "男", "男", "男", "女", "女", "男", "男", "女", "男", "女", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "女", "男", "男", "女", "女", "男", "男", "女", "女", "男", "男", "男", "男", "男", "男", "女", "男", "女", "女", "女", "女", "女", "女", "男", "女", "女", "女", "女", "女", "男", "男", "女", "女", "女", "男" };
+            int[] CM = { 173, 0, 179, 155, 183, 170, 163, 174, 165, 189, 177, 180, 154, 167, 170, 173, 165, 158, 180, 165, 153, 175, 162, 165, 0, 160, 165, 173, 164, 177, 177, 180, 170, 151, 176, 180, 168, 152, 666, 2147483647, 165, 155, 777, 173, 169, 170, 169, 171, 0, 170, 150, 160, 155, 164, 163, 165, 184, 165, 155, 0, 168, 160, 169, 0, 150, 163, 168, 173 };
 
-            int typeA = 0, typeB = 0, typeO = 0, typeAB = 0, typeElse = 0;
+            int typeA = 0, typeB = 0, typeO = 0, typeAB = 0, typeElse = 0, man = 0, woman = 0;
+            for (int x = 0; x < Gender.Length; x++)
+            {
+                if (Gender[x] == "男")
+                {
+                    man++;
+                }
+
+                else
+                {
+                    woman++;
+                }
+            }
+
             for (int i = 0; i < Type.Length; i++)
             {
                 if (Type[i] == "A")
@@ -37,11 +52,17 @@ namespace ConsoleApp1
                     typeElse++;
                 }
             }
+
+            float manp = (float)man / (float)Gender.Length;
+            float womanp = (float)woman / (float)Gender.Length;
             double typeAp = (double)typeA / (double)68;
             double typeOp = (double)typeO / (double)68;
             double typeBp = (double)typeB / (double)68;
             double typeABp = (double)typeAB / (double)68;
             double typeEp = (double)typeElse / (double)68;
+
+            Console.WriteLine("男生占全班比例:" + manp);
+            Console.WriteLine("女生占全班比例:" + womanp);
             Console.WriteLine("A型:" + typeA.ToString() + "占全班比例:" + typeAp);
             Console.WriteLine("B型:" + typeB.ToString() + "占全班比例:" + typeBp);
             Console.WriteLine("AB型:" + typeAB.ToString() + "占全班比例:" + typeABp);
@@ -49,23 +70,25 @@ namespace ConsoleApp1
             Console.WriteLine("其他:" + typeElse.ToString() + "占全班比例:" + typeEp);
 
 
-            string[] Gender = { "男", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "男", "女", "男", "男", "男", "女", "女", "男", "男", "女", "男", "女", "男", "男", "女", "男", "男", "女", "男", "男", "男", "男", "女", "男", "男", "女", "女", "男", "男", "女", "女", "男", "男", "男", "男", "男", "男", "女", "男", "女", "女", "女", "女", "女", "女", "男", "女", "女", "女", "女", "女", "男", "男", "女", "女", "女", "男" };
-            int [] CM = { 173, 0, 179, 155, 183, 170, 163, 174, 165, 189, 177, 180, 154, 167, 170, 173, 165, 158, 180, 165, 153, 175, 162, 165, 0, 160, 165, 173, 164, 177, 177, 180, 170, 151, 176, 180, 168, 152, 666, 2147483647, 165, 155, 777, 173, 169, 170, 169, 171, 0, 170, 150, 160, 155, 164, 163, 165, 184, 165, 155, 0, 168, 160, 169, 0, 150, 163, 168, 173 };
-
-            int girlCM = 0;
-            int girl = 0;
+           
+            int girlCM = 0,boyCM = 0;
             for (int a = 0; a < Gender.Length; a++)
             {
                 if (Gender[a] == "女" && CM[a] < 200 && CM[a] > 100)
-                {                    
-                    girl++;
+                {
                     girlCM += CM[a];
                 }
-                
+                else if (Gender[a] == "男" && CM[a] < 200 && CM[a] > 100)
+                {
+                    boyCM += CM[a];
+                }
             }
-            float girlCMavenger = (float)girlCM /(float)girl;
-            Console.WriteLine("女生身高:" + girlCM);
+            float girlCMavenger = (float)girlCM /(float)woman;
+            float boyCMavenger = (float)boyCM / (float)man;
+            float allCMavenger = (float)(boyCM + girlCM)/ (float)Gender.Length;
             Console.WriteLine("女生平均身高:" +girlCMavenger);
+            Console.WriteLine("男生平均身高:" + boyCMavenger);
+            Console.WriteLine("全班平均身高:" + allCMavenger);
             Console.Read();
         }
     }
